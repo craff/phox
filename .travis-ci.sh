5350cb0ba59cfe45c6c347f52a63efe87bf0f66d
craff <raffalli@univ-savoie.fr>
2017-09-30 19:41:33 +0200

    travis simplify

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,9 +1,6 @@
 # Edit this for your own project dependencies
 OPAM_DEPENDS="ocamlfind ocamlbuild"
 
-export OPAMYES=1
-opam switch ${OCAML_VERSION}
-eval `opam config env`
 opam install ${OPAM_DEPENDS}
 make
 sudo make install
