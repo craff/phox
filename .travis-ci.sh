07824e5ed9121b96cd4aacea85a8eb44a8ff5ad4
craff <raffalli@univ-savoie.fr>
2017-09-30 19:37:42 +0200

    travis more versions

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,9 +1,7 @@
 # Edit this for your own project dependencies
 OPAM_DEPENDS="ocamlfind ocamlbuild"
 
-sudo apt-get install -qq opam
 export OPAMYES=1
-opam init
 opam switch ${OCAML_VERSION}
 eval `opam config env`
 opam install ${OPAM_DEPENDS}
